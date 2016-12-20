776935950d79b09ee35be3a8c67f769af80f2f44
Florian Letanoux <letanoux.florian@gmail.com>
2016-12-20 21:57:16 +0100

    Dirty fix for input number not working on firefox

--- src/RFIEStatefulBase.js
+++ src/RFIEStatefulBase.js
@@ -39,7 +39,7 @@ export default class RFIEStatefulBase extends RFIEBase {
   componentDidUpdate = (prevProps, prevState) => {
     if (this.state.editing && !prevState.editing) {
       const inputElem = ReactDOM.findDOMNode(this.input);
-      inputElem.focus();
+      if(inputElem.type !== 'number') inputElem.focus();
       this.selectInputText(inputElem);
     } else if (this.state.editing && prevProps.text != this.props.text) {
       this.finishEditing();
